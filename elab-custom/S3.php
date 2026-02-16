c4e932d09bfe5ea218a610edd222fc71bc9d03cc
rikoimade <riko@imre.a-star.edu.sg>
2026-02-16 23:24:20 +0800

    fix syntax error

--- elab-custom/S3.php
+++ elab-custom/S3.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @author Nicolas CARPi <nico-git@deltablot.email>
  * @copyright 2022 Nicolas CARPi
@@ -7,7 +6,6 @@
  * @license AGPL-3.0
  * @package elabftw
  */
-
 declare(strict_types=1);
 
 namespace Elabftw\Storage;
@@ -20,12 +18,11 @@ use League\Flysystem\FilesystemAdapter;
 
 /**
  * Provide a League\Filesystem adapter for S3 buckets file uploads
+ * Patched for GCS Uniform Bucket-Level Access (no ACLs/visibility)
  */
 class S3 extends AbstractStorage
 {
     protected const string S3_VERSION = '2006-03-01';
-
-    // 100 Mb
     protected const int PART_SIZE = 104857600;
 
     public function __construct(
@@ -41,39 +38,35 @@ class S3 extends AbstractStorage
     {
         return 's3://' . $this->config->get('bucket_name') . '/' . $this->getPath($path);
     }
-    }
 
     protected function getAdapter(): FilesystemAdapter
     {
         $client = $this->getClient();
         $client->registerStreamWrapper();
+
         return new AwsS3V3Adapter(
-            // S3Client
             $client,
-            // Bucket name
             $this->config->get('bucket_name'),
-            // Optional path prefix
             $this->config->get('path_prefix'),
-            // set a larger part size for multipart upload or we hit the max number of parts (1000)
             options: ['part_size' => self::PART_SIZE],
         );
     }
 
     protected function getClient(): S3ClientInterface
     {
-        return new S3Client(array(
-            'version' => self::S3_VERSION,
-            'region' => $this->config->get('aws_region') ?? 'asia-southeast1',
-            'endpoint' => $this->config->get('aws_endpoint') ?? 'https://storage.googleapis.com',
+        return new S3Client([
+            'version'     => self::S3_VERSION,
+            'region'      => $this->config->get('aws_region') ?? 'asia-southeast1',
+            'endpoint'    => $this->config->get('aws_endpoint') ?? 'https://storage.googleapis.com',
             'credentials' => [
-                'key'    => $this->config->accessKey ?? '',
-                'secret' => $this->config->secretKey ?? '',
+                'key'    => $this->config->get('aws_access_key') ?? '',
+                'secret' => $this->config->get('aws_secret_key') ?? '',
             ],
             'use_aws_shared_config_files' => false,
             'use_path_style_endpoint'     => (bool) $this->config->get('aws_use_path_style_endpoint'),
             'http' => [
                 'verify' => (bool) $this->config->get('aws_verify_cert', true),
             ],
-        ));
+        ]);
     }
 }
